5e8cd6bfbd675585874011457d34490cdbbd2c2f
anh-nn01 <59980429+anh-nn01@users.noreply.github.com>
2025-09-21 03:47:49 +0000

    Deploying to gh-pages from @ anh-nn01/anh-nhu@2dd46ae444f8fd562b6226adf71af4eed95071c2 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,7 +18,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "This is my full CV, last updated on Sep. 10, 2025.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/anh-nhu/cv/";
